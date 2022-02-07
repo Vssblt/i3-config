508009f5c04973247d283cc1aac98a49c2118378
Vssblt <sunruiyangcp01@163.com>
2022-02-07 09:12:51 +0800

    fix bugs

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt install xbacklight polybar htop i3status
 cp .xinitc .Xresources $HOME
 sudo cp ./i3status.conf /etc/
-sudo cp ./i3/ $HOME/.config/
-sudo cp ./polybar/ $HOME/.config/
+sudo cp ./i3/ $HOME/.config/ -r
+sudo cp ./polybar/ $HOME/.config/ -r
 nohup polybar Vssblt >/dev/null 2>&1 &
