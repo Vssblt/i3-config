627ad931fc2d46738c81c9600bc6b27ccee13745
Vssblt <sunruiyangcp01@163.com>
2022-02-07 11:02:19 +0800

    fix bugs

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install xbacklight polybar htop i3status
-cp .xinitc .Xresources $HOME
+cp .xinitc .Xresources .init $HOME
 sudo cp ./i3status.conf /etc/
 sudo cp ./i3/ $HOME/.config/ -r
 sudo cp ./polybar/ $HOME/.config/ -r
