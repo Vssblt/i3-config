e681be605d7d0259eb8e718519322c5484fb5b04
Vssblt <sunruiyangcp01@gmail.com>
2023-01-05 16:09:38 +0800

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo apt install xbacklight polybar htop i3status
-cp .xinitc .Xresources .init $HOME
-sudo cp ./i3status.conf /etc/
-sudo cp ./i3/ $HOME/.config/ -r
-sudo cp ./polybar/ $HOME/.config/ -r
-nohup polybar Vssblt >/dev/null 2>&1 &
+sudo apt install xbacklight polybar htop rofi calc -y
+
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
+cd polybar-themes && ./setup.sh && cd -
+rm $HOME/.config/i3 -rf
+cp ./i3 $HOME/.config/ -r
+rm $HOME/.config/polybar/grayblocks
+cp ./grayblocks $HOME/.config/polybar/ -r
