8f9b896ad0e5d7876b35922def274f2087f05f41
Vssblt <sunruiyangcp01@gmail.com>
2023-01-25 20:16:02 +0800

    new commit

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 sudo apt install xbacklight polybar htop rofi calc -y
 
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
-cd polybar-themes && ./setup.sh && cd -
+cd polybar-themes && sudo ./setup.sh && cd -
 rm $HOME/.config/i3 -rf
 cp ./i3 $HOME/.config/ -r
-rm $HOME/.config/polybar/grayblocks
+rm $HOME/.config/polybar/grayblocks -rf
 cp ./grayblocks $HOME/.config/polybar/ -r
+
+mkdir ~/Pictures/ -p >/dev/null 2>&1
+cp ./wallpaper ~/Pictures/ -r
